22106985ae09a4e3f2c4786f3b21873ccdaa65a6
Manuel Torres <manueltorres1512@gmail.com>
2026-02-19 16:14:36 +0100

    Update spotify-token.js

--- api/spotify-token.js
+++ api/spotify-token.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+
+    console.log('CLIENT_ID:', process.env.CLIENT_ID)
+    console.log('CLIENT_SECRET:', process.env.CLIENT_SECRET)
+
     const response = await fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
@@ -6,5 +10,11 @@ export default async function handler(req, res) {
     })
 
     const data = await response.json()
+    console.log('Spotify response:', data)
+
+    if(!response.ok) {
+        return res.status(response.status).json({ error: data.error })
+    }
+
     res.status(200).json({ access_token: data.access_token })
 }
