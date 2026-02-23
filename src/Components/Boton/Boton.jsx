4ecc20e4b1f63694baeda475a144cd16d73537f3
Manuel Torres <manueltorres1512@gmail.com>
2026-02-23 21:29:39 +0100

    Update Boton.jsx

--- src/Components/Boton/Boton.jsx
+++ src/Components/Boton/Boton.jsx
@@ -3,8 +3,8 @@ import './Boton.css'
 export const Boton = ({name})=> {
 
     return(
-       <button className='spotify' style={{ width: 'fit-content', height: '100%', paddingLeft: 10, paddingRight: 10, paddingTop: 10, paddingBottom: 10, background: '#1ED760', borderRadius: 21, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex' }}>
-        <div style={{ textAlign: 'center', color: 'black', fontSize: 9, fontFamily: 'Avenir Next', fontWeight: '700', wordWrap: 'break-word' }}>{name}</div>
+       <button className='spotify btn'>
+        <div>{name}</div>
       </button> 
     )
 }
